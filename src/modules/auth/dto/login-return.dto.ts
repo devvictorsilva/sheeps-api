3f62a5bff2b68a5ca22d9f827a8fe423dbd971a1
Victor <paulo9.victor.silva@gmail.com>
2026-05-05 04:39:29 -0300

    feat: create login return dto

---
+++ src/modules/auth/dto/login-return.dto.ts
@@ -0,0 +1,9 @@
+export class LoginReturnDto {
+  accessToken!: string;
+  refreshToken!: string;
+
+  constructor(accessToken: string, refreshToken: string) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
+}
