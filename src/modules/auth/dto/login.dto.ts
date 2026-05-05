776a15e0be7bd1a91d55f7d789e22cea6b584ade
Victor <paulo9.victor.silva@gmail.com>
2026-05-05 04:58:46 -0300

    chore: add properties examples

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,9 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString } from "class-validator";
 
 export class LoginDto {
+  @ApiProperty({
+    example: 'user@test.com'
+  })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({
+    example: '12345',
+  })
   @IsString()
   password!: string;
 }
