27acbdb90a3e59a55ca994a5b0472bf589495ad1
Victor <paulo9.victor.silva@gmail.com>
2026-05-05 04:59:05 -0300

    docs: add return informations

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginReturnDto } from './dto/login-return.dto';
 import { LoginDto } from './dto/login.dto';
@@ -7,6 +8,25 @@ import { LoginDto } from './dto/login.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'User login',
+    description: 'Endpoint to user login in the app',
+  })
+  @ApiResponse({ 
+    status: 201, 
+    description: 'Login successful',
+    example: {
+      accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFydGh1ckBzaGVlcHMuY29tIiwic3ViIjoxLCJpYXQiOjE3Nzc5NjY2MDgsImV4cCI6MTc3ODA1MzAwOH0.K64FxoaPqsIhUy6HoW8U1rGqHe0nJ0sN9PHUZGpIB02',
+      refreshToken: 'ishafduiaflhdsjifhpas9udfp9a7sdyufioaiçdsjfho7'
+    }})
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    example: { message: 'Email ou senha inválidos' }
+  })
+  @ApiBody({
+    type: LoginDto,
+  })
   @Post('login')
   create(@Body() loginDto: LoginDto): Promise<LoginReturnDto> {
     return this.authService.login(loginDto);
